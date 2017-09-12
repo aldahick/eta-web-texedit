0e7bc091a9c90619d36fd8e46239177642968bc1
Alex Hicks <aldahick@iupui.edu>
2017-09-12 16:01:36 -0400

    Implement Ace support(?)

--- static/js/tex/edit.ts
+++ static/js/tex/edit.ts
@@ -1,11 +1,14 @@
 /// <reference path="../def/katex-autorender.d.ts" />
+import "ace";
 import * as katex from "katex";
 (<any>window).katex = (<any>katex).default;
 import renderMathInElement from "katex-autorender";
 
+let editor: AceAjax.Editor;
 function render(): void {
     const output: HTMLElement = document.getElementById("output");
-    output.innerText = $("#input").val().toString();
+    const value: string = editor.getValue();
+    output.innerText = value;
     renderMathInElement(output, {
         delimiters: [{
             left: "$",
@@ -15,11 +18,14 @@ function render(): void {
     });
     $.post("/api/tex/updateNote", {
         noteId: $("#note-id").val(),
-        body: $("#input").val()
+        body: value
     });
 }
 
 $(document).ready(() => {
+    editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+    editor.getSession().setMode("ace/mode/latex");
     $("#render").on("click", render);
     $(window).on("keydown", evt => {
         if (!(evt.ctrlKey && evt.which === JQuery.Key.S) && evt.which !== 19) return true;
